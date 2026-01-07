b4168b857998f3c63a79f42c5e24fe986a2d2fec
orik250 <orik5@nycstudents.net>
2026-01-07 13:26:39 -0500

    Refactor input generation to create 50 lines
    Modified the input generation logic to produce 50 lines of random characters instead of 10.

--- inputGenrator.java
+++ inputGenrator.java
@@ -2,17 +2,16 @@ public class inputGenrator {
     public static void main(String[] args){
         String alphabet = "abcdefghijklmnopqrstuvwxyz";
         String input = "";
-
+    for(int a=0; a<50; a++){
         for (int i=0; i<=9; i++){
-            for (int a=0; a<=9; a++){
-                int randint = (int)(Math.random()*26);
-                System.out.println("number: "+randint);
-                input+=alphabet.substring(randint, randint+1);
 
-            }
-            System.out.println(" ");
-            System.out.print(input);
+            int randint = (int)(Math.random()*26);
+            input+=alphabet.substring(randint, randint+1);
+
         }
-
+        System.out.print(input);
+        input = "";
+        System.out.println(" ");
+    }
     }
 }
