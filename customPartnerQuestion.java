2ee0741b1eb62c7c743f534f7ccab1063998efb2
orik250 <orik5@nycstudents.net>
2026-01-13 14:04:03 -0500

    Add files via upload

---
+++ customPartnerQuestion.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class customPartnerQuestion {
+    public static void main(String[] args){
+        ArrayList<String> lines = getFileData("src/data");
+        System.out.println(lines);
+        int total = 0;
+        String[] gifts = (lines.get(0)).split(",");
+        for (String g : gifts) {
+            System.out.println(g.trim());
+            String a = g.trim();
+            ArrayList<String> aList = new ArrayList<>();
+            for (int u=0; u<a.length(); u++){
+                aList.add(a.substring(u, u+1));
+            }
+            for(int e=0; e<aList.size(); e++){
+                String checkNum = aList.get(e);
+                System.out.println(aList.get(e));
+                System.out.println("CheckNum: "+checkNum);
+                for(int z=0; z<aList.size();z++){
+                    String compNum = aList.get(z);
+                    System.out.println("Compnum: "+compNum);
+                    if(checkNum.equals(compNum)&&(!(z==e))){
+                        System.out.println("Condition found to be "+checkNum.equals(compNum));
+                        total+=Integer.parseInt(a);
+
+                    }
+                }
+
+            }
+            System.out.println(aList);
+            System.out.println(total);
+
+        }
+
+    }
+    public static ArrayList<String> getFileData(String fileName) {
+        ArrayList<String> fileData = new ArrayList<>();
+        try {
+            File f = new File(fileName);
+            Scanner s = new Scanner(f);
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+                if (!line.equals(""))
+                    fileData.add(line);
+            }
+            return fileData;
+        }
+        catch (FileNotFoundException e) {
+            return fileData;
+        }
+    }
+}
