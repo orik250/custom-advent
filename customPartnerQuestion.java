1c6b5e98a3caafef01c2ed7d759ad0556398acea
orik250 <orik5@nycstudents.net>
2026-01-29 07:07:08 -0500

    Add files via upload

--- customPartnerQuestion.java
+++ customPartnerQuestion.java
@@ -8,44 +8,54 @@ public class customPartnerQuestion {
         ArrayList<String> lines = getFileData("src/data");
         System.out.println(lines);
         int total = 0;
-        int counter = 0;
-        String[] gifts = (lines.get(0)).split(",");
-        for (String g : gifts) {
-            System.out.println(g.trim());
-            String a = g.trim();
-            ArrayList<String> aList = new ArrayList<>();
-            for (int u=0; u<a.length(); u++){
-                aList.add(a.substring(u, u+1));
-            }
-            for(int e=0; e<aList.size(); e++){
-                System.out.println("e equals "+e);
-                String checkNum = aList.get(e);
-                for(int z=0; z<aList.size();z++){
-                    System.out.println("e equals "+e);
-                    System.out.println("z equals "+z);
-                    String compNum = aList.get(z);
+
+        for(int h=0; h< lines.size(); h++){
+            String[] gifts = (lines.get(h)).split(",");
+
+            for (String g : gifts) {
+                System.out.println(g.trim());
+                String a = g.trim();
+                ArrayList<String> aList = new ArrayList<>();
+
+                for (int u=0; u<a.length(); u++){
+                    aList.add(a.substring(u, u+1));
+                }
+
+                boolean damaged = false;
+
+                for(int e=0; e<aList.size(); e++){
+                    String checkNum = aList.get(e);
+                    System.out.println(aList.get(e));
                     System.out.println("CheckNum: "+checkNum);
-                    System.out.println("Compnum: "+compNum);
-                    if(checkNum.equals(compNum)&&(!(z==e))){
-                        System.out.println("Condition found to be "+checkNum.equals(compNum));
-                        System.out.println("current num at: "+g.trim());
-                        if(counter==0){
-                            counter++;
-                            total+=Integer.parseInt(g.trim());
-                            System.out.println("added current num to total");
+
+                    for(int z=0; z<aList.size();z++){
+                        String compNum = aList.get(z);
+                        System.out.println("Compnum: "+compNum);
+
+                        if(checkNum.equals(compNum)&&(!(z==e))){
+                            System.out.println("Condition found to be "+checkNum.equals(compNum));
+                            damaged = true;
+                            break;
                         }
+                    }
 
-
+                    if (damaged){
+                        break;
                     }
                 }
 
+                if (damaged) {
+                    total += Integer.parseInt(a);
+                }
+
+                System.out.println(aList);
+                System.out.println(total);
             }
-            System.out.println(aList);
-            counter=0;
-            System.out.println("counter back at "+counter);
         }
-        System.out.println("total: "+total);
+
+        System.out.println("Final total: "+total);
     }
+
     public static ArrayList<String> getFileData(String fileName) {
         ArrayList<String> fileData = new ArrayList<>();
         try {
